8fd6b7da4afac3e7a1eb23f70a688c646349f07d
lyanaTougon <lyana.tougon@gmail.com>
2026-03-17 15:54:43 +0100

    test

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+# Connexion MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client['movies_db']
+movies_collection = db['movies']
+
 @app.route('/')
 def home():
-    return "Cine-Note est prêt !"
+    movies = list(movies_collection.find())
+    return render_template('index.html', movies=movies)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
